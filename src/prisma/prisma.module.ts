cb92f868c1d0eae78c1c94b43edb9de2420dc2c0
Guy de Messimy <gdemessimy@gmail.com>
2023-03-31 23:14:16 +0200

    init: prisma service

---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  // Assumes that env variables are retrieved from the config module.
+  // imports: [ConfigModule],
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class PrismaModule {}
