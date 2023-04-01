f3026031a6d23e82f85a7faacf499a07e5413fbd
Guy de Messimy <gdemessimy@gmail.com>
2023-04-01 11:57:26 +0200

    cofig app module

---
+++ src/modules/posts/posts.model.ts
@@ -0,0 +1,26 @@
+import { Field, GraphQLISODateTime, Int, ObjectType } from '@nestjs/graphql';
+import { Post as PostDB } from '@prisma/client';
+
+@ObjectType({ description: 'Post model' })
+export class Tweet {
+  @Field(() => Int)
+  id: PostDB[`id`];
+
+  @Field(() => GraphQLISODateTime)
+  createdAt: PostDB[`createdAt`];
+
+  @Field(() => GraphQLISODateTime)
+  updatedAt: PostDB[`updatedAt`];
+
+  @Field(() => String)
+  content: PostDB[`content`];
+
+  @Field(() => String)
+  title: PostDB[`title`];
+
+  @Field(() => Int)
+  authorId: PostDB[`authorId`];
+
+  @Field(() => Boolean)
+  published: PostDB[`published`];
+}
--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  // Assumes that env variables are retrieved from the config module.
-  // imports: [ConfigModule],
   providers: [PrismaService],
   exports: [PrismaService],
 })
