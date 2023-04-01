ceef08f2fc3c128c0f5643e371587b7fc667fa54
Guy de Messimy <gdemessimy@gmail.com>
2023-04-01 22:22:25 +0200

    init primsa in main ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,10 @@ async function bootstrap() {
     .setDescription('farming application')
     .setVersion('1.0')
     .build();
+
+  // initialize prisma service
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
