3656d251da5af00f3599e43a550e67096654997e
Guy de Messimy <gdemessimy@gmail.com>
2023-04-02 22:05:53 +0200

    config pubsub

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PostsModule } from './modules/posts/posts.module';
-import { ApiModule } from 'src/api/api.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { PubSubModule } from './modules/pub-sub/pub-sub.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
+      installSubscriptionHandlers: true,
     }),
     PostsModule,
     PrismaModule,
+    PubSubModule,
   ],
   controllers: [],
   providers: [],
