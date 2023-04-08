351a573e4ff63655d195793a9c41b7e6f61f6a2d
Guy de Messimy <gdemessimy@gmail.com>
2023-04-08 17:43:05 +0200

    feat : use global pipes

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,28 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { PrismaService } from './prisma/prisma.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // have error message in case of incorrect request
+      disableErrorMessages: false,
+      // filter out properties that should not be received by the method handler and removed it
+      whitelist: true,
+      //  stop a request if any non-white listed properties are present
+      forbidNonWhitelisted: false,
+      // transform payloads on an instance of their dto
+      transform: true,
+      // perform conversion of primitive types comes from the network (ex request id = string to number)
+      // if true @type on DTO can be disable
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
   // Setting up Swagger document
   const options = new DocumentBuilder()
     .setTitle('FarmingMachine')
